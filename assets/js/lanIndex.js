e2c82a236c7c8c286fc6758db74d2c930ea08d4b
Dawid Olko <dawid_olko@outlook.com>
2024-04-19 05:07:13 +0200

    update

--- assets/js/lanIndex.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const languageSelector = document.querySelector('select[name="language"]');
-
-  languageSelector.addEventListener("change", function () {
-    const selectedLanguage = this.value;
-
-    document.documentElement.setAttribute("lang", selectedLanguage);
-  });
-});
